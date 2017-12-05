fdc2e8e4e2af892ef10a8d8a18e2d113b23de271
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-05 15:16:01 -0700

    Reorganized elements

--- src/App/Components/DataBox/DataBox.js
+++ src/App/Components/DataBox/DataBox.js
@@ -4,20 +4,18 @@ import CardContainer from '../CardContainer/CardContainer';
 import './DataBox.css';
 
 const DataBox = () => {
-    return(
-        <div className='DataBox'>
-            <header>ST4R W4RS 4PI<Button value='View Favorites' /></header>
-            <div>
-                <Button
-                    value='People' />
-                <Button
-                    value='Planets' />
-                <Button
-                    value='Vehicles' />
-            </div>
-            <CardContainer />
-        </div>
-    )
-}
+  return (
+    <div className="DataBox">
+      <Button value="View Favorites" />
+      <header className="header">SWAPI-Box</header>
+      <div className="button-box">
+        <Button value="People" />
+        <Button value="Planets" />
+        <Button value="Vehicles" />
+      </div>
+      <CardContainer />
+    </div>
+  );
+};
 
 export default DataBox;
