590b9a2e541b0ca5c0d65ffdd1dc5300eb75a55e
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-04 20:47:49 -0700

    Build eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "jest": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "newline-after-var": [
+        "error",
+        "always"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
