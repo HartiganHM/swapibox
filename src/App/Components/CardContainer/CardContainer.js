ab8583bb99757157805c07a9c18cc911eba7b252
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 15:44:02 -0700

    Conditional display of span with no data

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card';
 import './CardContainer.css';
 
 const CardContainer = ({ category }) => {
-    const cards = category.map( obj => <Card data={obj}/>);
+    const cards = category ? category.map( obj => <Card data={obj}/>) : <span>Select a category</span>
 
     return(
         <div className='card-container'>
