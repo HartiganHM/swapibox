baab42678accb09643fec33074b1e5cbd8e6d5ec
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-05 20:49:11 -0700

    Fix spacing

--- src/App/Components/Header/Header.js
+++ src/App/Components/Header/Header.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import './Header.css';
 
-const Header = ( { crawlData }) => {
+const Header = ({ crawlData }) => {
+  const romanNumeral = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII'
+  };
 
-    const romanNumeral = {
-        1: 'I',
-        2: 'II',
-        3: 'III',
-        4: 'IV',
-        5: 'V',
-        6: 'VI',
-        7: 'VII'
-    }
-
-    return(
-        <div className='Header'>
-            <div className='crawl'>
-                <div className='title'>
-                    <span>Episode {romanNumeral[crawlData.episodeNum]}</span>
-                    <span>{crawlData.episodeTitle}</span>
-                </div>
-                <p>{crawlData.openingCrawl}</p>
-            </div>
+  return (
+    <div className="Header">
+      <div className="crawl">
+        <div className="title">
+          <span>Episode {romanNumeral[crawlData.episodeNum]}</span>
+          <span>{crawlData.episodeTitle}</span>
         </div>
-    )
-}
+        <p>{crawlData.openingCrawl}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
