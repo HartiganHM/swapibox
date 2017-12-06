b67179e012e8456008367a09765d8760e2bf3ff2
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-05 20:36:56 -0700

    Deconstruct crawlData and pass to JSX; construct romanNumeral obj

--- src/App/Components/Header/Header.js
+++ src/App/Components/Header/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = ( { episodeNum, episodeTitle, openingCrawl }) => {
+
+    const romanNumeral = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII'
+    }
+
     return(
         <div className='Header'>
             <div className='crawl'>
                 <div className='title'>
-                    <span>Episode IV</span>
-                    <span>A New Hope</span>
+                    <span>Episode {romanNumeral.episodeTitle}</span>
+                    <span>{episodeTitle}</span>
                 </div>
                 <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
                 <p>During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
