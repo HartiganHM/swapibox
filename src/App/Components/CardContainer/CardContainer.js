14e7d8f45d55b6f8d966ce20c767bae2f8420d2c
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 14:40:44 -0700

    Map over data to display Cards and send respective data

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -2,19 +2,12 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = () => {
+const CardContainer = ({ category }) => {
+    const cards = category.map( obj => <Card data={obj}/>);
+
     return(
         <div className='card-container'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {cards}
         </div>
     )
 }
