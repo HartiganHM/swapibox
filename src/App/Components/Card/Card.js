42a668e171e06b2ebf29694c8cf4858c4dc5e8e3
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-05 21:33:37 -0700

    Mock render card information

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -4,7 +4,16 @@ import './Card.css';
 const Card = () => {
     return(
         <div className='Card'>
-            Me a card!
+            <div className='card-header'>
+                <span className='card-title'>Luke SkyWalker</span>
+                <button className='mark-favorite'>★</button>
+            </div>
+            <ul className='card-info'>
+                <li className='card-data-set'>Homeworld: Tatooine</li>
+                <li className='card-data-set'>Species: Human</li>
+                <li className='card-data-set'>Language: Galactic Basic</li>
+                <li className='card-data-set'>Population: 200,000</li>
+            </ul>
         </div>
     )
 }
