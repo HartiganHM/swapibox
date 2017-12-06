60660ef65d31f367da87477763113b4afceefc91
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 13:28:56 -0700

    Fix spacing

--- src/App/Components/Button/Button.js
+++ src/App/Components/Button/Button.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({ type, value }) => {
-    return(
-        <button className={ type }>{ value } <span className='counter'>{ value === 'View Favorites' ? 0 : '' }</span></button>
-    )
-}
+  return (
+    <button className={type}>
+      {value}{' '}
+      <span className="counter">{value === 'View Favorites' ? 0 : ''}</span>
+    </button>
+  );
+};
 
 export default Button;
