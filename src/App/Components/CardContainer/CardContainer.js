7be98d840182baf3f4410b6019b1885ce4010a9e
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-05 21:07:49 -0700

    Temp styling for Card and fix file names

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './CardContainer.css';
 
 const CardContainer = () => {
     return(
         <div className='card-container'>
             <span className='placeholder'>Select a Category</span>
+            <Card />
         </div>
     )
 }
