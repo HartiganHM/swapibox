d3d77a064a070e03dffff3715db1fb137473ccd6
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 14:40:54 -0700

    Deconstruct and place data

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -2,18 +2,18 @@ import React from 'react';
 import icon from '../../../Images/Icons/death-star.svg';
 import './Card.css';
 
-const Card = () => {
+const Card = ({ data }) => {
     return(
         <div className='Card'>
             <div className='card-header'>
-                <span className='card-title'>Luke SkyWalker</span>
+                <span className='card-title'>{ data.name }</span>
                 <button className='mark-favorite'><img src={icon}/></button>
             </div>
             <ul className='card-info'>
-                <li className='card-data-set'>Homeworld: Tatooine</li>
-                <li className='card-data-set'>Species: Human</li>
-                <li className='card-data-set'>Language: Galactic Basic</li>
-                <li className='card-data-set'>Population: 200,000</li>
+                <li className='card-data-set'>Homeworld: { data.data.homeworld }</li>
+                <li className='card-data-set'>Species: { data.data.species }</li>
+                <li className='card-data-set'>Language: { data.data.language }</li>
+                <li className='card-data-set'>Population: { data.data.population }</li>
             </ul>
         </div>
     )
