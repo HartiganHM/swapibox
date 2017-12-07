31a9f4255d7270062d1bc730940ccc5da3b9cd48
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 13:17:50 -0700

    Fix spacing and pass removeFavorite to Card

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -2,9 +2,21 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({ category, currentDisplay, toggleFavorite }) => {
+const CardContainer = ({
+  category,
+  currentDisplay,
+  toggleFavorite,
+  removeFavorite
+}) => {
   const cards = category ? (
-    category.map(obj => <Card data={obj} currentDisplay={currentDisplay} toggleFavorite={toggleFavorite} />)
+    category.map(obj => (
+      <Card
+        data={obj}
+        currentDisplay={currentDisplay}
+        toggleFavorite={toggleFavorite}
+        removeFavorite={removeFavorite}
+      />
+    ))
   ) : (
     <span className="placeholder">Select a category</span>
   );
