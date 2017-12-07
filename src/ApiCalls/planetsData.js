4070f042892f7b74a8f320e88200a96fe9e24c81
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 08:36:41 -0700

    Move fetch and clean function to separate file, format

---
+++ src/ApiCalls/planetsData.js
@@ -0,0 +1,30 @@
+const fetchPlanets = async () => {
+    const fetchedPlanets = await fetch('https://swapi.co/api/planets/');
+    const jsonPlanets = await fetchedPlanets.json();
+    const planets = this.cleanPlanetsData(jsonPlanets.results);
+
+    return planets;
+}
+
+const cleanPlanetsData = (planets) => {
+    const unresolvedPromises = planets.map(async planet => {
+      let residents = planet.residents.map(async resident => {
+        let fetchedResident = await fetch(resident);
+        let jsonResident = await fetchedResident.json();
+
+        return jsonResident.name;
+      });
+
+      return {
+        name: planet.name,
+        data: {
+          terrain: planet.terrain,
+          population: planet.population,
+          climate: planet.climate,
+          residents: await Promise.all(residents)
+        }
+      };
+    });
+
+    return Promise.all(unresolvedPromises);
+};
