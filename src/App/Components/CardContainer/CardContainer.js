a287446c2af88c3b3b224d915561892d87716623
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 22:12:38 -0700

    Add placeholder class to span

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card';
 import './CardContainer.css';
 
 const CardContainer = ({ category }) => {
-    const cards = category ? category.map( obj => <Card data={obj}/>) : <span>Select a category</span>
+    const cards = category ? category.map( obj => <Card data={obj}/>) : <span className='placeholder'>Select a category</span>
 
     return(
         <div className='card-container'>
