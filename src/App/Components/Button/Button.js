9783b3f3d2314a9cf15e4e2b24d79ea448dac0d9
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-06 21:41:30 -0700

    Write buttonClass conditional for active button class of selected

--- src/App/Components/Button/Button.js
+++ src/App/Components/Button/Button.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({ type, value, selectData }) => {
+const Button = ({ type, value, selectData, currentDisplay }) => {
+  const buttonClass = value.toLowerCase() === currentDisplay ? `${type} selected` : type;
+
   return (
-    <button className={ type } onClick={ () => selectData(value) }>
+    <button className={buttonClass} onClick={() => selectData(value)}>
       {value}{' '}
       <span className="counter">{value === 'View Favorites' ? 0 : ''}</span>
     </button>
