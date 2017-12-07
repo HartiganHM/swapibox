60be07b052068c581099493c4add545fb4c15e4b
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 08:35:28 -0700

    Fetch successful, remove old code

--- src/App/App.js
+++ src/App/App.js
@@ -26,36 +26,6 @@ class App extends Component {
     this.setState({ crawlData, people, planets, vehicles });
   }
 
-  fetchPeople = async () => {
-    const fetchedPeople = await fetch('https://swapi.co/api/people/');
-    const jsonData = await fetchedPeople.json();
-    const people = this.cleanPeopleData(jsonData.results);
-
-    return people;
-  };
-
-  cleanPeopleData(people) {
-    const unresolvedPromises = people.map(async person => {
-      let homeworldFetch = await fetch(person.homeworld);
-      let homeworldData = await homeworldFetch.json();
-
-      let speciesFetch = await fetch(person.species);
-      let speciesData = await speciesFetch.json();
-
-      return {
-        name: person.name,
-        data: {
-          homeworld: homeworldData.name,
-          species: speciesData.name,
-          language: speciesData.language,
-          population: homeworldData.population
-        }
-      };
-    });
-
-    return Promise.all(unresolvedPromises);
-  }
-
   fetchPlanets = async () => {
     const fetchedPlanets = await fetch('https://swapi.co/api/planets/');
     const jsonPlanets = await fetchedPlanets.json();
