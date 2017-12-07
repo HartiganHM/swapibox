83f1d18dc330f56c92a6b1a6b8e05a5a090b51b4
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 16:24:44 -0700

    Fix formatting

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -2,24 +2,11 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({
-  category,
-  currentDisplay,
-  toggleFavorite,
-  removeFavorite
-}) => {
+const CardContainer = ({ category, currentDisplay, toggleFavorite, removeFavorite }) => {
   const cards = category ? (
     category.map(obj => (
-      <Card
-        data={obj}
-        currentDisplay={currentDisplay}
-        toggleFavorite={toggleFavorite}
-        removeFavorite={removeFavorite}
-      />
-    ))
-  ) : (
-    <span className="placeholder">Select a category</span>
-  );
+      <Card data={obj} currentDisplay={currentDisplay} toggleFavorite={toggleFavorite} removeFavorite={removeFavorite}/>)))
+      : (<span className="placeholder">Select a category</span>);
 
   return <div className="card-container">{cards}</div>;
 };
