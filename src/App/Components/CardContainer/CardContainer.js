e0b85ef9f945001a1bcdfcb5fbd28dca8ec10197
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-08 07:50:01 -0700

    Remove typo

--- src/App/Components/CardContainer/CardContainer.js
+++ src/App/Components/CardContainer/CardContainer.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = ({ category, currentDisplay, currentFavorites, saveFavoriter, removeFavorite }) => {
+const CardContainer = ({ category, currentDisplay, currentFavorites, saveFavorite, removeFavorite }) => {
   const cards = category ? (
     category.map(obj => (
-      <Card data={obj} currentDisplay={currentDisplay} currentFavorites={currentFavorites} saveFavoriter={saveFavoriter} removeFavorite={removeFavorite}/>)))
+      <Card data={obj} currentDisplay={currentDisplay} currentFavorites={currentFavorites} saveFavorite={saveFavorite} removeFavorite={removeFavorite}/>)))
       : '';
     const message = currentDisplay === 'favorites' ? (<span className="placeholder">No favorites to display</span>) : (<span className="placeholder">Select a category</span>);
 
