7cff11b4a8e7bca23e2b2f53f8b31e91d5678923
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 21:19:08 -0700

    Write conditional statement to determine if Card if marked as favorite

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ data, toggleFavorite, removeFavorite, currentDisplay }) => {
+const Card = ({ data, currentFavorites, toggleFavorite, removeFavorite, currentDisplay }) => {
+  const type = Object.values(currentFavorites).find( card => card === data ) ? 'Card selected-favorite' : 'Card';
   const clickFunction = currentDisplay === 'favorites' ? removeFavorite : toggleFavorite;
   const dataPoints = Object.keys(data.data).map(dataPoint => (
     <li className="card-data-set">
-      {dataPoint}: {data.data[dataPoint].length ? data.data[dataPoint] : 0}
+      { dataPoint }: { data.data[dataPoint].length ? data.data[dataPoint] : 0 }
     </li>
   ));
 
   return (
-    <div className="Card">
+    <div className={type}>
       <div className="card-header">
         <span className="card-title">{data.name}</span>
         <div className='button-container'>
-            <button className="mark-favorite" onClick={() => clickFunction(data)}>
+            <button
+                className="mark-favorite"
+                onClick={ () => clickFunction(data) }>
             </button>
         </div>
       </div>
