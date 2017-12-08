2a8d249be59a22aa69c9deeeb0bc5109a475fe26
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-08 08:52:47 -0700

    Switch clickFunction on Card based on className instead of display section

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 const Card = ({ data, currentFavorites, saveFavorite, removeFavorite, currentDisplay }) => {
   const type = Object.values(currentFavorites).find( card => card === data ) ? 'Card selected-favorite' : 'Card';
   const favorited = type === 'Card' ? 'mark-favorite' : 'mark-favorite current-favorite';
-  const clickFunction = currentDisplay === 'favorites' ? removeFavorite : saveFavorite;
+  const clickFunction = type === 'Card selected-favorite' ? removeFavorite : saveFavorite;
   const dataPoints = Object.keys(data.data).map(dataPoint => (
     <li className='card-data-set'>
       <span className='data-key'>{ dataPoint }</span>: { data.data[dataPoint].length ? data.data[dataPoint] : 0 }
