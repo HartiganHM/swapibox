6551787bc00d0225fccc5e0c135b23bfd0ca4ede
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-07 21:26:55 -0700

    Refactor favorite button conditional styling

--- src/App/Components/Card/Card.js
+++ src/App/Components/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = ({ data, currentFavorites, toggleFavorite, removeFavorite, currentDisplay }) => {
   const type = Object.values(currentFavorites).find( card => card === data ) ? 'Card selected-favorite' : 'Card';
-  const favorited = Object.values(currentFavorites).find( card => card === data ) ? 'mark-favorite current-favorite' : 'mark-favorite';
+  const favorited = type === 'Card' ? 'mark-favorite' : 'mark-favorite current-favorite';
   const clickFunction = currentDisplay === 'favorites' ? removeFavorite : toggleFavorite;
   const dataPoints = Object.keys(data.data).map(dataPoint => (
     <li className="card-data-set">
