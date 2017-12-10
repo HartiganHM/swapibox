bd387d23a32f3c3e8b1b9851ddd1b89495106df4
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 13:36:50 -0700

    Build test file and complete tests

---
+++ src/ApiCalls/planetsData.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import fetchPlanets from './planetsData';
+
+window.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        results: [
+          {
+            name: 'Tatooine',
+            terrain: 'desert',
+            population: '200000',
+            climate: 'arid',
+            residents: []
+          }
+        ]
+      })
+  })
+);
+
+describe('Planets Data Tests', () => {
+  it('Should render correctly', () => {
+    expect(fetchPlanets).toBeAFunction;
+  });
+
+  it('Should be called with the correct parameters', async () => {
+    const planetsData = await fetchPlanets();
+    const fetchApi = 'https://swapi.co/api/planets/';
+
+    expect(window.fetch).toHaveBeenCalledWith(fetchApi);
+  });
+
+  it('Should return an object when called', async () => {
+    const planetsData = await fetchPlanets();
+
+    expect(typeof planetsData).toEqual('object');
+  });
+
+  it('Should return a cleaned object', async () => {
+    const planetsData = await fetchPlanets();
+    const expectedObject = [
+      {
+        name: 'Tatooine',
+        list: {
+          Terrain: 'desert',
+          Population: '200000',
+          Climate: 'arid',
+          Residents: ''
+        }
+      }
+    ];
+
+    expect(planetsData).toEqual(expectedObject);
+  });
+});
