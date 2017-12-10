c56c20321d26c3784a673edfd36603b569c46c05
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 13:38:50 -0700

    Adjusted watch.js

--- src/ApiCalls/peopleData.test.js
+++ src/ApiCalls/peopleData.test.js
@@ -5,18 +5,17 @@ window.fetch = jest.fn().mockImplementation(() =>
   Promise.resolve({
     json: () =>
       Promise.resolve({
+        status: 200,
         results: [
-            {
-                name: 'Luke Skywalker',
-                    list: {
-                        Homeworld: 'Tatooine',
-                        Species: 'Human',
-                        Language: 'Galactic Basic',
-                        Population: '200000'
-                    }
-            }
+          {
+            name: 'Luke Skywalker',
+            homeworld: {},
+            species: [],
+            language: '',
+            population: ''
+          }
         ]
-    })
+      })
   })
 );
 
@@ -29,29 +28,29 @@ describe('People Data Tests', () => {
     const peopleData = await fetchPeople();
     const fetchApi = 'https://swapi.co/api/people/';
 
-    expect(window.fetch).toHaveBeenCalledWith(fetchApi)
+    expect(window.fetch).toHaveBeenCalledWith(fetchApi);
   });
 
   it('Should return an object when called', async () => {
-      const peopleData = await fetchPeople();
+    const peopleData = await fetchPeople();
 
-      expect(typeof peopleData).toEqual('object');
+    expect(typeof peopleData).toEqual('object');
   });
 
-  it('Should return a cleaned object' , async () => {
-      const peopleData = await fetchPeople();
-      const expectedObject = [
-        {
-            name: 'Luke Skywalker',
-                list: {
-                    Homeworld: undefined,
-                    Species: undefined,
-                    Language: undefined,
-                    Population: undefined
-                }
+  it('Should return a cleaned object', async () => {
+    const peopleData = await fetchPeople();
+    const expectedObject = [
+      {
+        name: 'Luke Skywalker',
+        list: {
+          Homeworld: undefined,
+          Species: undefined,
+          Language: undefined,
+          Population: undefined
         }
-    ]
+      }
+    ];
 
-      expect(peopleData).toEqual(expectedObject);
+    expect(peopleData).toEqual(expectedObject);
   });
 });
