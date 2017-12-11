4eb6dac112aeacf2b8685f4ca6bedcc25561dc63
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 23:01:53 -0700

    Refactor fetchHomeworld

--- src/ApiCalls/peopleData/peopleData.js
+++ src/ApiCalls/peopleData/peopleData.js
@@ -10,10 +10,15 @@ const fetchPeople = async () => {
   }
 };
 
+const fetchHomeworld = async homeworld => {
+  let homeworldFetch = await fetch(homeworld);
+  let homeworldData = await homeworldFetch.json();
+  return homeworldData;
+};
+
 const cleanPeopleData = people => {
   const unresolvedPromises = people.map(async person => {
-    let homeworldFetch = await fetch(person.homeworld);
-    let homeworldData = await homeworldFetch.json();
+    let homeworld = await fetchHomeworld(person.homeworld);
 
     let speciesFetch = await fetch(person.species);
     let speciesData = await speciesFetch.json();
@@ -21,10 +26,10 @@ const cleanPeopleData = people => {
     return {
       name: person.name,
       list: {
-        Homeworld: homeworldData.name,
+        Homeworld: homeworld.name,
         Species: speciesData.name,
         Language: speciesData.language,
-        Population: homeworldData.population
+        Population: homeworld.population
       }
     };
   });
