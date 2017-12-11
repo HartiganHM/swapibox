67d7fcb8ad4e485d4534634ebe2fcfcd17b8b699
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 21:00:12 -0700

    Finish Header testing suite

---
+++ src/App/Components/Header/Header.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './Header';
+import { shallow } from 'enzyme';
+
+describe('Header Tests', () => {
+    let renderedHeader;
+    let mockCrawlData;
+
+    beforeEach( () => {
+        mockCrawlData = {
+            episodeNum: 7,
+            episodeTitle: 'Star Trek N Stuff',
+            openingCrawl: 'Me crawl!'
+        }
+        renderedHeader = shallow(
+            <Header crawlData={mockCrawlData}/>
+        )
+    });
+
+    it('Should exist', () => {
+        expect(renderedHeader).toBeDefined();
+    });
+
+    it('Should match the snapshot', () => {
+        expect(renderedHeader).toMatchSnapshot();
+    });
+
+    it('Should convert the episode number to a Roman numeral', () => {
+        const mockEpisodeNumber = renderedHeader.find('span').first();
+        const expectedText = 'Episode VII';
+
+        expect(mockEpisodeNumber.text()).toEqual(expectedText);
+    })
+})
