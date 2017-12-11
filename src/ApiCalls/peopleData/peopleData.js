db58b0f5572cf16b101216ffc16ae2ea8f1e210d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 23:04:37 -0700

    Refacotr fetchSpecies

--- src/ApiCalls/peopleData/peopleData.js
+++ src/ApiCalls/peopleData/peopleData.js
@@ -11,24 +11,28 @@ const fetchPeople = async () => {
 };
 
 const fetchHomeworld = async homeworld => {
-  let homeworldFetch = await fetch(homeworld);
-  let homeworldData = await homeworldFetch.json();
+  const homeworldFetch = await fetch(homeworld);
+  const homeworldData = await homeworldFetch.json();
   return homeworldData;
+};
+
+const fetchSpecies = async species => {
+  const speciesFetch = await fetch(species);
+  const speciesData = await speciesFetch.json();
+  return speciesData;
 };
 
 const cleanPeopleData = people => {
   const unresolvedPromises = people.map(async person => {
     let homeworld = await fetchHomeworld(person.homeworld);
-
-    let speciesFetch = await fetch(person.species);
-    let speciesData = await speciesFetch.json();
+    let species = await fetchSpecies(person.species);
 
     return {
       name: person.name,
       list: {
         Homeworld: homeworld.name,
-        Species: speciesData.name,
-        Language: speciesData.language,
+        Species: species.name,
+        Language: species.language,
         Population: homeworld.population
       }
     };
