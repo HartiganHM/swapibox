1a7c359f018649fa20a79c95b9eb8c87a2a85eb8
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-10 19:19:13 -0700

    Finish Card test suite and update Button snapshots

---
+++ src/App/Components/Card/Card.test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Card from './Card';
+import { shallow } from 'enzyme';
+
+describe('Card Tests', () => {
+  let renderedCard;
+  let mockDisplayData;
+  let mockCurrentFavorites;
+  let mockSaveFavorites;
+  let mockRemoveFavorites;
+
+  beforeEach(() => {
+    mockDisplayData = {
+      name: 'Luke Skywalker',
+      list: {
+        Homeworld: 'Tatooine',
+        Species: 'Human',
+        Language: 'Intergalactic Basic',
+        Population: '20000000'
+      }
+    };
+    mockCurrentFavorites = [];
+    mockSaveFavorites = jest.fn();
+    mockRemoveFavorites = jest.fn();
+
+    renderedCard = shallow(
+      <Card
+        displayData={mockDisplayData}
+        currentFavorites={mockCurrentFavorites}
+        saveFavorite={mockSaveFavorites}
+        removeFavorite={mockRemoveFavorites}
+      />
+    );
+  });
+
+  it('Should exist', () => {
+    expect(renderedCard).toBeDefined();
+  });
+
+  it('Search should match the snapshot', () => {
+    expect(renderedCard).toMatchSnapshot();
+  });
+
+  it('Should have have a list item for each data point', () => {
+    expect(renderedCard.find('.card-data-set').length).toEqual(4);
+  });
+
+  it('Should have a class of selected-favorite if the card exists in favorites', () => {
+    mockCurrentFavorites = [mockDisplayData];
+    renderedCard = shallow(
+      <Card
+        displayData={mockDisplayData}
+        currentFavorites={mockCurrentFavorites}
+        saveFavorite={mockSaveFavorites}
+      />
+    );
+
+    expect(renderedCard.find('.selected-favorite').length).toEqual(1);
+  });
+
+  it('Should have a removeFavorite button if in favorites', () => {
+    expect(renderedCard.find('.current-favorite').length).toEqual(0);
+
+    mockCurrentFavorites = [mockDisplayData];
+    renderedCard = shallow(
+      <Card
+        displayData={mockDisplayData}
+        currentFavorites={mockCurrentFavorites}
+        saveFavorite={mockSaveFavorites}
+      />
+    );
+
+    expect(renderedCard.find('.current-favorite').length).toEqual(1);
+  });
+
+  it('Should show a 0 for any list item with no value', () => {
+    mockDisplayData = {
+      name: 'Luke Skywalker',
+      list: {
+        Population: ''
+      }
+    };
+    renderedCard = shallow(
+      <Card
+        displayData={mockDisplayData}
+        currentFavorites={mockCurrentFavorites}
+        saveFavorite={mockSaveFavorites}
+      />
+    );
+
+    expect(renderedCard.find('.card-data-set').text()).toEqual('Population: 0');
+  });
+});
